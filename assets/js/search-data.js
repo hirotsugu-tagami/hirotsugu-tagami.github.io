94341761111728c0ef7ccec76a7e7f7b40b906d9
hirotsugu-tagami <60811251+hirotsugu-tagami@users.noreply.github.com>
2025-07-11 11:52:01 +0000

    Deploying to gh-pages from @ hirotsugu-tagami/hirotsugu-tagami.github.io@89b7066c8056e949bc82dab713cdfa751a6f2b67 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -453,35 +453,28 @@ ninja.data = [{
         title: 'Send an email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%69%72%6F%74%73%75%67%75@%67.%65%63%63.%75-%74%6F%6B%79%6F.%61%63.%6A%70", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/hirotsugu-tagami", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/hirotsugu-tagami-97264b373", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/tag_phys", "_blank");
         },
       },{
           id: 'lang-en-us',
